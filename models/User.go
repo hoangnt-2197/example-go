9a95545c7f324c7d5973336dc0802065ba7506a8
Nguyen Trong Hoang (hoangnt-2197) <nguyen.trong.hoang@sun-asterisk.com>
2020-09-20 22:08:06 -0900

    add hash password

--- models/User.go
+++ models/User.go
@@ -1,9 +1,13 @@
 package models
 
 import (
+	"errors"
 	"example/config"
 	"fmt"
+	"github.com/badoux/checkmail"
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
+	 "strings"
 )
 
 func GetAllUsers(user *[]User) (err error) {
@@ -45,4 +49,58 @@ func GetUserByUsernameAndPassword(user *User) (err error) {
 	}
 	return nil
 }
+
+func Hash(password string) ([]byte, error) {
+  return  bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword,password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (user *User) BeforeSave() error{
+	hashedPassword, err := Hash(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	return nil
+}
+
+func (user *User) Validate(action string) error  {
+	switch strings.ToLower(action) {
+	case "update":
+		if user.Name== ""{
+			return errors.New("required name")
+		}
+		if user.Email == "" {
+			return errors.New("required email")
+		}
+        if err := checkmail.ValidateFormat(user.Email); err!=nil {
+        	return errors.New("invalid email")
+		}
+		return nil
+	case "login":
+		if user.Password== ""{
+			return errors.New("required password")
+		}
+		if user.Username == "" {
+			return errors.New("required username")
+		}
+		return nil
+	default:
+		if user.Name== ""{
+			return errors.New("required name")
+		}
+		if user.Email == "" {
+			return errors.New("required email")
+		}
+		if err := checkmail.ValidateFormat(user.Email); err!=nil {
+			return errors.New("invalid email")
+		}
+		return nil
+	}
+
+}
+
 
--- models/UserModel.go
+++ models/UserModel.go
@@ -1,15 +1,24 @@
 package models
 
 type User struct {
+	Id			uint 	`gorm:"primary_key; auto_increment" json:"id"`
+	Name		string 	`gorm:"size:255; not null" json:"name"`
+	Email		string	`gorm:"size:100; not null; unique" json:"email"`
+	Phone		string	`json:"phone"`
+	Address		string	`gorm:"size:255" json:"address"`
+	Username 	string 	`gorm:"size:100; not null; unique" json:"username"`
+	Password 	string	`gorm:"size:100;not null;" json:"password"`
+}
+
+type UserResponse struct {
 	Id			uint 	`json:"id"`
 	Name		string 	`json:"name"`
 	Email		string	`json:"email"`
 	Phone		string	`json:"phone"`
-	Address		string	`json:"address"` 
+	Address		string	`json:"address"`
 	Username 	string 	`json:"username"`
-	Password 	string	`json:"password"`
 }
 
-func (b *User)  TableName() string{
+func (user *User)  TableName() string{
 	return "user"
 }
